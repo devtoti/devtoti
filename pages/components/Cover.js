1d0bc4fa613ee4178aa09904ac82a993332482e8
devtoti <96400225+devtoti@users.noreply.github.com>
2022-07-12 19:06:33 -0500

    Animate moon svg: user can now interact with the div's position

--- pages/components/Cover.js
+++ pages/components/Cover.js
@@ -1,8 +1,18 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import styles from '../../styles/_cover.module.scss'
 import ForestBg from '../../public/svg/ForestBg'
+import {handleMLeave, handleMUp, changeSize } from './functions'
+import Moon4 from "../../public/images/moon4.png";
 export default function Cover() {
-  
+  const [objCoords, setObjCoords] = useState({});
+  const [isDragging, setIsDragging] = useState(false);
+  const [percentage, setPercentage] = useState(0);
+  const [objStyle, setObjStyle] = useState({
+    position: "absolute",
+    height: "650px",
+    width: "650px",
+  });
+
   const [winHeight, setWinHeight] = useState(0)
   useEffect(() => {
     // window is accessible here.
@@ -10,17 +20,58 @@ export default function Cover() {
     // console.log("window.innerHeight", window.innerHeight);
   }, []);
 
+  const handleDrag = (event) => {
+    const elem = event.target;
+    let windowWidth = window.innerWidth;
+    setPercentage((event.pageX * 100) / windowWidth);
+    const coords = elem.getBoundingClientRect();
+    const elemX = parseInt(event.target.style.width, 10);
+    const elemY = parseInt(event.target.style.height, 10);
+
+   if (isDragging) {
+      setObjStyle({
+        ...objStyle,
+        top: event.pageY - elemY / 2 + "px",
+        left: event.pageX - elemX / 2 + "px",
+        filter: "drop-shadow(0 0 0.75rem #fff1cc)",
+        transform: `scale(${0.45 + percentage / 100})`
+      });
+    }
+  };
+
+  const handleMDown = (event) => {
+    setIsDragging(true);
+    const elem = event.target;
+    const coords = elem.getBoundingClientRect();
+    console.log(coords)
+    setObjCoords(coords);
+  };
+  const handleMUp = (event) => {
+    setIsDragging(false);
+    // setObjCoords(event.target.getBoundingClientRect());
+  };
+
+
   return (
-    <div className={styles.container} style={{minHeight: winHeight}}>
+    <div className={styles.container} style={{ minHeight: winHeight }}>
       <section>
         <h2><span>_</span>devtoti</h2>
-      <h4>web portfolio</h4>
+        <h4>web portfolio</h4>
       </section>
       <ForestBg />
-      <div className={styles.moon}>
+      <div
+        className={styles.moon}
+        onMouseDown={handleMDown}
+        onMouseMove={handleDrag}
+        onMouseUp={handleMUp}
+        style={objStyle}
+      >
         <div className={styles.outer}></div>
         <div className={styles.inner}></div>
       </div>
     </div>
   )
+
+ 
+
 }
