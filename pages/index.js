fa9cbe93713658ad7d005d427b4d17b34e0607e6
devtoti <96400225+devtoti@users.noreply.github.com>
2022-07-22 21:10:17 -0500

    Fix JSX syntax errors

--- pages/index.js
+++ pages/index.js
@@ -91,15 +91,15 @@ export default function Home() {
   const SvgPath = () => {
     return (
       <svg viewBox="0 0 1509 5" fill="none" preserveAspectRatio='xMidYMin meet' className={styles.linePath}>
-        <path d="M0 2.5H1509" stroke="url(#paint0_linear_67_21)" stroke-miterlimit="1.41421" strokeWidth={winHeight > 768 ? "10" : "20"} ref={pathRef}
+        <path d="M0 2.5H1509" stroke="url(#paint0_linear_67_21)" strokeMiterlimit="1.41421" strokeWidth={winHeight > 768 ? "10" : "20"} ref={pathRef}
           style={{
             strokeDasharray: `${currPathLength + ' ' + currPathLength}`,
             strokeDashoffset: currStrokeOffset,
           }} />
         <defs>
           <linearGradient id="paint0_linear_67_21" y1="754.5" x1="2.5" y2="754.5" x2="3.5" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#D9CCAF" />
-            <stop stop-color="#F2C94C" />
+            <stop stopColor="#D9CCAF" />
+            <stop stopColor="#F2C94C" />
           </linearGradient>
         </defs>
       </svg>
